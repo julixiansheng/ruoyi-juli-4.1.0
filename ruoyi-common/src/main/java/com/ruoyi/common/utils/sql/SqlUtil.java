f21accac6c8eca9ef8c4d7032149d599fcad1696
居里先生 <798224073@qq.com>
2019-11-26 10:26:28 +0800

    sql工具类新增过滤like eq查询条件方法

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/sql/SqlUtil.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/sql/SqlUtil.java
@@ -4,11 +4,10 @@ import com.ruoyi.common.utils.StringUtils;
 
 /**
  * sql操作工具类
- * 
+ *
  * @author ruoyi
  */
-public class SqlUtil
-{
+public class SqlUtil {
     /**
      * 仅支持字母、数字、下划线、空格、逗号（支持多个字段排序）
      */
@@ -17,10 +16,8 @@ public class SqlUtil
     /**
      * 检查字符，防止注入绕过
      */
-    public static String escapeOrderBySql(String value)
-    {
-        if (StringUtils.isNotEmpty(value) && !isValidOrderBySql(value))
-        {
+    public static String escapeOrderBySql(String value) {
+        if (StringUtils.isNotEmpty(value) && !isValidOrderBySql(value)) {
             return StringUtils.EMPTY;
         }
         return value;
@@ -29,8 +26,40 @@ public class SqlUtil
     /**
      * 验证 order by 语法是否符合规范
      */
-    public static boolean isValidOrderBySql(String value)
-    {
+    public static boolean isValidOrderBySql(String value) {
         return value.matches(SQL_PATTERN);
     }
+
+    /**
+     * 处理用于like查询字符串中的特殊字符
+     *
+     * @param str 处理前的字符串
+     * @return 处理后的字符串
+     */
+    public static String dealSpecialCharUseLike(String str) {
+        if (null == str || "".equals(str.trim())) {
+            return "";
+        }
+        str = str.replace("'", "''");
+        char[] chars = str.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '%' || chars[i] == '_' || chars[i] == '/') {
+                sb.append("/");
+            }
+            sb.append(chars[i]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 处理用于=查询字符串中的特殊字符
+     *
+     * @param str 处理前的字符串
+     * @return 处理后的字符串
+     */
+    public static String dealSpecialCharUseEq(String str) {
+        return str.replace("'", "''");
+    }
+
 }
