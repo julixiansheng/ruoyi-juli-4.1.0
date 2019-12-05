52fec1925fc6f5b2ff9a576a5d26fbbb63ecb6a3
居里先生 <798224073@qq.com>
2019-12-05 12:23:46 +0800

    添加jackrabbit gc的任务调度方法

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
@@ -1,13 +1,25 @@
 package com.ruoyi.quartz.task;
 
-import org.springframework.stereotype.Component;
 import com.ruoyi.common.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.jackrabbit.api.JackrabbitRepository;
+import org.apache.jackrabbit.api.JackrabbitRepositoryFactory;
+import org.apache.jackrabbit.api.management.DataStoreGarbageCollector;
+import org.apache.jackrabbit.api.management.RepositoryManager;
+import org.apache.jackrabbit.core.RepositoryFactoryImpl;
+import org.springframework.stereotype.Component;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import java.util.Properties;
 
 /**
- * 定时任务调度测试
+ * 定时任务调度
  * 
  * @author yuanxy
  */
+@Slf4j
 @Component("ryTask")
 public class RyTask
 {
@@ -24,5 +36,43 @@ public class RyTask
     public void ryNoParams()
     {
         System.out.println("执行无参方法");
+    }
+
+    /**
+     * jackrabbit GC垃圾回收，清理storedata区域垃圾文件
+     *
+     * @param jackrabbitDir
+     */
+    public void jackrabbitGC(String jackrabbitDir){
+        try {
+            JackrabbitRepositoryFactory rf = new RepositoryFactoryImpl();
+            Properties prop = new Properties();
+
+            prop.setProperty("org.apache.jackrabbit.repository.home", jackrabbitDir);
+            prop.setProperty("org.apache.jackrabbit.repository.conf", jackrabbitDir + "/repository.xml");
+
+            JackrabbitRepository rep = (JackrabbitRepository) rf.getRepository(prop);
+            RepositoryManager rm = rf.getRepositoryManager(rep);
+
+            // need to login to start the repository
+            Session session = rep.login(new SimpleCredentials("admin", "admin".toCharArray()));
+
+            //数据存储垃圾回收器
+            DataStoreGarbageCollector gc = rm.createDataStoreGarbageCollector();
+            try {
+                //标记
+                gc.mark();
+                //清理
+                gc.sweep();
+            } finally {
+                gc.close();
+            }
+
+            session.logout();
+            rm.stop();
+        }catch (RepositoryException e){
+            log.error("调用jackrabbit GC垃圾回收器失败!", e);
+        }
+        log.info("调用jackrabbit GC垃圾回收器成功!");
     }
 }
